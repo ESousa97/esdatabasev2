31a0c833844a2e13554170a5a09afc6fad41b671
Sousa <114963739+ESousa97@users.noreply.github.com>
2025-02-25 21:35:36 -0300

    fix: Correção de bug na página de procedimentos

--- componentes/Procedures/ProcedurePages.js
+++ componentes/Procedures/ProcedurePages.js
@@ -12,7 +12,7 @@ function ProcedurePages() {
 
   useEffect(() => {
     if (id) {
-      axios.get(`https://serverdatabase.vercel.app/api/procedure.js?id=${id}`)
+      axios.get(`serverdatabase.vercel.app/api/procedure?id=${id}`)
         .then(response => {
           setProcedure(response.data);
         })
